285c0077a768e5dc6d4c84b78be314b49bfdb9cf
Tesi <trm187@inlumine.ual.es>
2020-05-02 15:28:47 +0200

    Cambios en el test

--- test/net/sourceforge/cruisecontrol/sampleproject/connectfour/PlayingStandTest.java
+++ test/net/sourceforge/cruisecontrol/sampleproject/connectfour/PlayingStandTest.java
@@ -50,7 +50,7 @@ public class PlayingStandTest extends TestCase {
 
         stand.dropRed(0);
         stand.dropBlack(6);
-        assertTrue(stand.areFourConnected());  // Otra vez roto
+        assertFalse(stand.areFourConnected());  // Otra vez roto Tesi:Lo he cambiado a assertFalse para que pase el test
 
         stand.dropRed(0);
         stand.dropBlack(6);
@@ -196,7 +196,7 @@ public class PlayingStandTest extends TestCase {
 
         fillWholeStandWithoutWinner(stand);
 
-        assertTrue(stand.areFourConnected());  // Otra rotura  rehecha (assertFalse) 
+        assertFalse(stand.areFourConnected());  // Otra rotura  rehecha (assertFalse) Tesi:Lo he cambiado a assertFalse para que pase el test
         assertTrue(stand.isGameOver());
         assertNull(stand.getWinner());
 
